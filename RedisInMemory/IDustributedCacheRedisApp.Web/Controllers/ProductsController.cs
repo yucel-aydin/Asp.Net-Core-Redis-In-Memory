845f6ceaf98a5b978684654bb008ffff91bd3cd9
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-31 11:41:01 +0300

    Complex Data Kaydetme

--- RedisInMemory/IDustributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ RedisInMemory/IDustributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDustributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace IDustributedCacheRedisApp.Web.Controllers
 {
@@ -12,16 +15,56 @@ namespace IDustributedCacheRedisApp.Web.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            #region Basit data kaydetme
+            //DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            //cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+            //_distributedCache.SetString("name", "yücel", cacheEntryOptions);
+            //await _distributedCache.SetStringAsync("surname", "aydın", cacheEntryOptions);
+            #endregion
+
+            #region Complex Data Kaydetme
+            // Nesne Kaydetme
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
-            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _distributedCache.SetString("name", "yücel", cacheEntryOptions);
-            await _distributedCache.SetStringAsync("surname", "aydın", cacheEntryOptions);
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+            Product product = new Product()
+            {
+                Id = 3,
+                Name = "Kitap 3",
+                Price = 500
+            };
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            await _distributedCache.SetStringAsync("product:2", jsonProduct, cacheEntryOptions);
+
+            // Binary Kaydetme
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            await _distributedCache.SetAsync("product:3", byteProduct, cacheEntryOptions);
+
+            #endregion
             return View();
         }
         public async Task<IActionResult> Show()
         {
-            ViewBag.name = _distributedCache.GetString("name");
-            ViewBag.surname = await _distributedCache.GetStringAsync("surname");
+            #region Complex Data Kaydetme
+            //Json datayı alma
+            string jsonProduct = _distributedCache.GetString("product:1");
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.product = product;
+
+            // binary datayı alma (daha meşakatli iş olduğu için json tipini kullanmak daha iyidir.)
+            Byte[] byteProduct = _distributedCache.Get("product:3");
+            string json = Encoding.UTF8.GetString(byteProduct);
+            Product product3 = JsonConvert.DeserializeObject<Product>(json);
+
+
+            #endregion
+
+            #region Basit data kaydetme
+            //ViewBag.name = _distributedCache.GetString("name");
+            //ViewBag.surname = await _distributedCache.GetStringAsync("surname");
+            #endregion
+
             return View();
         }
         public IActionResult Remove()
