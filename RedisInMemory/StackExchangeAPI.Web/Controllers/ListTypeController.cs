7a36ec7c55535d3d0daa883f5818ef532c664482
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-11-01 17:34:37 +0300

    Redis List

---
+++ RedisInMemory/StackExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeAPI.Web.Services;
+
+namespace StackExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "names";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _redisService.Connect();
+            db = _redisService.GetDb(1);
+        }
+        public IActionResult Index()
+        {
+            List<string> namesList = new List<string>();
+            // Keyexists ile key varsa bool döner
+            if (db.KeyExists(listKey))
+            {
+
+                db.ListRange(listKey).ToList().ForEach(x =>
+                {
+                    namesList.Add(x.ToString());
+                });
+            }
+
+            return View(namesList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            db.ListLeftPush(listKey, name);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.ListRemoveAsync(listKey, name).Wait();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFirstItem()
+        {
+            db.ListLeftPop(listKey);
+            return RedirectToAction("Index");
+        }
+    }
+}
