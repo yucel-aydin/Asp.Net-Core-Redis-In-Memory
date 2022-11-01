f3bdd06614fa27929921e0bbe694696e8e7329fa
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-11-01 18:20:12 +0300

    Redis Hash

---
+++ RedisInMemory/StackExchangeAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeAPI.Web.Services;
+
+namespace StackExchangeAPI.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        protected readonly IDatabase db;
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _redisService.Connect();
+            db = _redisService.GetDb(1);
+        }
+    }
+}
---
+++ RedisInMemory/StackExchangeAPI.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeAPI.Web.Services;
+
+namespace StackExchangeAPI.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase db;
+
+        private string listKey = "sortedsetnames";
+
+        public SortedSetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _redisService.Connect();
+            db = _redisService.GetDb(3);
+        }
+
+        public IActionResult Index()
+        {
+            HashSet<string> list = new HashSet<string>();
+
+            if (db.KeyExists(listKey))
+            {
+                db.SortedSetScan(listKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+
+                db.SortedSetRangeByRank(listKey, 0, 5, order: Order.Descending).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, int score)
+        {
+            db.SortedSetAdd(listKey, name, score);
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(1));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.SortedSetRemove(listKey, name);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
